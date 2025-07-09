c2e020bca3d60fc9fe3ae9651c2cd9932deff3d8
micdynas <orjimicheal133@gmail.com>
2025-07-09 10:48:40 +0100

    new file:   practice/images/hero-bg.jpg 	new file:   practice/images/newsletter-bg.jpg 	new file:   practice/images/recipe1.jpg 	new file:   practice/images/recipe2.jpg 	new file:   practice/images/recipe3.jpg 	new file:   practice/images/recipe4.jpg 	new file:   practice/index.html 	new file:   practice/script.js 	new file:   practice/style.css

---
+++ practice/script.js
@@ -0,0 +1,11 @@
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = body.getAttribute('data-theme');
+    if (currentTheme === 'dark') {
+        body.removeAttribute('data-theme');
+    } else {
+        body.setAttribute('data-theme', 'dark');
+    }
+});
