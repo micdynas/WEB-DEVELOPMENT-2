b9a560dd9f332372837f11e1dcc066fd2cae98f9
micdynas <orjimicheal133@gmail.com>
2025-11-25 10:46:53 +0100

    new file:   .vscode/settings.json 	modified:   Simon Game Challenge Starting Files/index.js

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -2,53 +2,21 @@ var buttonColors = ["red", "blue", "green", "yellow" ];
 var gamePattern = [];
 var userClickedPattern = [];
 
-var level = 0;
-var gameStarted = false;
-
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 3) + 1;
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    console.log("nextSequence() called!");
    
-
- $(".btn").on("click", function(){
-
-            $("#" + randomChosenColor).animate({ opacity: 0.2 }, 100) 
+  $("#" + randomChosenColor).animate({ opacity: 0.2 }, 100) 
            .animate({ opacity: 1 }, 100)
            .animate({ opacity: 0.2 }, 100)
            .animate({ opacity: 1 }, 100); 
         
-     
-         });
-
-
-
-
-level++;
+ 
 $("#level-title").text("Level " + level);
-  
-
-
+ level++;
 }
-
-nextSequence();
-      
-$(document).on("keyDown", function() {
-  if (!gameStarted) {
-     $("#level-title").text("Level " + level);
-     nextSequence();
-    gameStarted = true;
-  }
-  else{
-nextSequence();
-  }
-});
-        
-
-
-
 
 
 // to play sound when clicked
@@ -102,7 +70,64 @@ $(".btn").on("click", function () {
          }
 animatePress();
 
+// To start the game 
+var gameStarted = false;
+$(document).on("keydown", function() {
+  if (!gameStarted) {
+    nextSequence();
+    gameStarted = true;
+  }});
+
+var level = 0;
+
+// Check answer
+function checkAnswer(currentLevel) {
+  // Check if the last answer the user clicked matches the game's pattern
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    // If it's correct, check if the user has completed the full sequence
+    if (userClickedPattern.length === gamePattern.length) {
+      console.log("success");
+      // If successful, move to the next level after a delay
+      setTimeout(nextSequence, 1000);
+    } else {
+      console.log("correct");
+      // User is correct but the sequence isn't over
+    }
+  } 
+  else {
+    // Game over logic would go here
+      $("body").addClass("game-over");
+      setTimeout(function() {
+            $("body").removeClass("game-over");
+          }, 100);
+        
+          $("#level-title").text("Game Over, Press Any Key to Restart");
+      
+          var wrong1 = new Audio("sounds/wrong.mp3");
+           wrong1.play();
+           
+           
+
+        
+       
+      }
+   }
 
 
 
+$(".btn").on("click", function() {
+  checkAnswer(userClickedPattern.length);
+});
+ 
+    //restart the game
+          //  function startOver() {
+          //   var level = 0;
+          //   var gamePattern = [];
+          //   var gameStarted = false;
+          //  $("#level-title").text("Press A Key to Start");
 
+
+
+          //  };
+
+  
